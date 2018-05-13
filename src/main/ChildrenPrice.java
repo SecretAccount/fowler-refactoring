886bad5eda3c20019a38ad4e09fcaef9de54c467
Pascal Meßner <pasnicom@web.de>
2018-05-14 01:47:25 +0200

    Price.java: [Replace Conditional with Polymorphism] moved getCharge to each price subclass

--- src/main/ChildrenPrice.java
+++ src/main/ChildrenPrice.java
@@ -2,7 +2,16 @@ package main;
 
 public class ChildrenPrice extends Price {
     @Override
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.CHILDREN;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+
+        return result;
     }
 }
--- src/main/NewReleasePrice.java
+++ src/main/NewReleasePrice.java
@@ -2,7 +2,12 @@ package main;
 
 public class NewReleasePrice extends Price {
     @Override
-    int getPriceCode() {
+    public int getPriceCode() {
         return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented * 3;
     }
 }
