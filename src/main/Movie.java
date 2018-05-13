78b62411ed23fa59142d332699fbe2fa45513f56
Pascal Meßner <pasnicom@web.de>
2018-05-14 01:34:01 +0200

    Customer.java: [Move Method] getCharge() to Rental.java

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -23,4 +23,24 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    double getCharge(int daysRenteed) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRenteed > 2)
+                    result += (daysRenteed - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRenteed * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRenteed > 3)
+                    result += (daysRenteed - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
